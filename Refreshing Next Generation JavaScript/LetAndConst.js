0a15697a2926aa13cb20c3eb7c3cecac3d4fec3a
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-11 12:55:15 +0200

    LetAndConst
    Difference

---
+++ Refreshing Next Generation JavaScript/LetAndConst.js
@@ -0,0 +1,12 @@
+const nome="Pedro";
+console.log(nome);
+//nome="Fred"; //wouldnt work
+console.log(nome);
+//It should give an error message because 
+//we use const for final statements
+
+let name="Fred";
+console.log(name);
+name="Pedro"; //would work
+console.log(name);
+//we use let for variable statements
