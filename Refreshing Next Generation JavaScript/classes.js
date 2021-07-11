899adb87e7724315fb5d3d98740c90d2da9cfdbe
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-11 16:30:02 +0200

    Classes

---
+++ Refreshing Next Generation JavaScript/classes.js
@@ -0,0 +1,26 @@
+console.log("=================");
+console.log("Lecture: Classes");
+class Human {
+    constructor() {
+        this.gender = "male";
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = "Max";
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
