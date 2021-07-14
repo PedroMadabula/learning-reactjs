98c4dbf666d9b3feadf56321786ba91c6a80a30b
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-14 23:20:34 +0200

    corrections

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+    return React.createElement('div',{className: 'App'},React.createElement('h1',null,'It works now!'));
   }
 }
 
