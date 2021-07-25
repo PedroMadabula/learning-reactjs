fd487f31c37a1d247a26b29d34221f34391a0037
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-25 12:41:29 +0200

    Props
    Using props in React components to set attributes in our jsx elements

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div',{className: 'App'},React.createElement('h1',null,'It works now!'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29"/>
+        <Person name="Fred" age="3">I'm gonna be a great Full Stack Dev by the end of this Year</Person>
+      </div>
+    );
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'It works now!'));
   }
 }
 
