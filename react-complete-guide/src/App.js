dde6abfb188cb6ff9deef90d4ad8b221bc33c0da
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-31 09:47:33 +0200

    using React Hooks

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+    const [personsState, setPersonsState] = useState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: 'Manu', age: 29},
+        { name: 'Stephanie', age: 26}
+      ],
+      otherState: 'Some other value'
+    }); 
+
+    console.log(personsState);
+
+    const switchNameHandler = () => {
+      console.log('Was clicked');
+      // personsState.persons[0].name = 'Pedro';
+      setPersonsState({
+        persons: [
+          { name: 'Pedro', age: 28},
+          { name: 'Manu', age: 29},
+          { name: 'Frederico', age: 29}
+       ]
+     });
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29"/>
-        <Person name="Fred" age="3">I'm gonna be a great Full Stack Dev by the end of this Year</Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>I'm gonna be a great Full Stack Dev by the end of this Year</Person>
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'It works now!'));
-  }
 }
 
-export default App;
+export default app;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,7 +5,6 @@ const person = (props) => {
         <div>
             <p>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            
         </div>
     )
 };
