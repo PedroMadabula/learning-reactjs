0924b278f3c599a7ed9ff1079d20e1a3207884fb
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-07-31 10:05:48 +0200

    bind

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,12 +15,12 @@ const app = props => {
 
     console.log(personsState);
 
-    const switchNameHandler = () => {
+    const switchNameHandler = (newName) => {
       console.log('Was clicked');
       // personsState.persons[0].name = 'Pedro';
       setPersonsState({
         persons: [
-          { name: 'Pedro', age: 28},
+          { name: newName, age: 28},
           { name: 'Manu', age: 29},
           { name: 'Frederico', age: 29}
        ]
@@ -31,10 +31,17 @@ const app = props => {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>I'm gonna be a great Full Stack Dev by the end of this Year</Person>
+        <button onClick={switchNameHandler.bind(this, 'Pedrocas')}>Switch Name</button>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}/>
+        <Person 
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}/>
+        <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}
+          click = {switchNameHandler.bind(this, 'Pedrocas')}> I'm gonna be a great Full Stack Dev by the end of this Year</Person>
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'It works now!'));
