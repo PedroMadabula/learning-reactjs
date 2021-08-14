48cd34a01a3d9bb370a1f6d85899197991cabf75
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2021-08-14 13:52:36 +0200

    Styling components

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Person.css';
+import styled from 'styled-components';
 
-const person = (props) => {
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px sold #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+        width: 450px;
+    }
+`;
+
+const person = (props) => {  
+
     return (
-        <div className='Person'>
+        <StyledDiv>
+        {/* //styled tem todos os elementos como metodos */}
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
+        </StyledDiv>
     )
 };
 
